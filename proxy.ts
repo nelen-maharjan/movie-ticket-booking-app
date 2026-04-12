4b72f946f830ecb5055a00059b17c8b011aed2fd
Nelen Maharjan <neleen11@gmail.com>
2026-04-12 15:01:28 +0545

    middleware

--- proxy.ts
+++ proxy.ts
@@ -1,1 +1,37 @@
-export { auth as proxy } from "@/lib/auth";
+import { auth } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/dashboard")) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  if (pathname.startsWith("/admin")) {
+    if (!isLoggedIn || req.auth?.user?.role !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+
+  if (pathname.startsWith("/login") && isLoggedIn) {
+  const role = req.auth?.user?.role;
+
+  if (role === "admin") {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
+
+  // default for normal users
+  return NextResponse.redirect(new URL("/", req.url));
+}
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/admin/:path*", "/login"],
+};
