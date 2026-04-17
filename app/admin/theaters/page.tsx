7b62dbccd74fa67e7144aa3cc00a5db946ba502c
Nelen Maharjan <neleen11@gmail.com>
2026-04-17 11:35:48 +0545

    theaters fix

--- app/admin/theaters/page.tsx
+++ app/admin/theaters/page.tsx
@@ -1,8 +1,13 @@
 import { getTheaters } from "@/app/actions/showtimes";
 import { AdminTheatersClient } from "@/components/admin/AdminTheatersClient";
 
-
 export default async function AdminTheatersPage() {
   const theaters = await getTheaters();
-  return <AdminTheatersClient theaters={theaters} />;
+
+  const normalized = theaters.map((t) => ({
+    ...t,
+    phone: t.phone ?? undefined,
+  }));
+
+  return <AdminTheatersClient theaters={normalized} />;
 }
